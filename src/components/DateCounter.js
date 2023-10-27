ffbd16b1bdfe8e3397bf5b5407d010d75d5bc3a1
Koko-Dev <koko.webdev@gmail.com>
2023-10-27 11:47:57 -0400

    DateCounter.js:  Add a counter which increments and decrements by the click of a button, displays the date and resets the count and step states

---
+++ src/components/DateCounter.js
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+
+
+function DateCounter() {
+  const [ count, setCount ] = useState( 0 );
+  const [ step, setStep ] = useState( 1 );
+
+//  This mutates the date object.
+  const date = new Date( 'june 21, 2027' );
+  
+  const dec = function () {
+    setCount( prevState => prevState - step );
+  };
+  
+  const inc = function () {
+    setCount( prevState => prevState + step );
+  };
+  
+  const defineCount = function ( e ) {
+    setCount( Number( e.target.value ) );
+  };
+  
+  const defineStep = function ( e ) {
+    setStep( Number( e.target.value ) );
+  };
+  
+  const reset = function () {
+    setCount( 0 );
+    setStep( 1 );
+  };
+  
+  return (
+      <div className='counter'>
+        <div>
+          <input
+              type='text'
+              min='0'
+              max='10'
+              value={ step }
+              onChange={ defineStep }
+          />
+          <span>{ step }</span>
+        </div>
+        
+        <div>
+          <button onClick={ dec }>-</button>
+          <input
+              type='text'
+              value={ count }
+              onChange={ defineCount }
+          />
+          <button onClick={ inc }>+</button>
+        </div>
+        
+        <p>{ date.toDateString() }</p>
+        
+        <div>
+          <button onClick={ reset }>Reset</button>
+        </div>
+      </div>
+  );
+}
+
+
+export default DateCounter;
