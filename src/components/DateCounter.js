e675fd2aebb4416705bd25b67bc0eab7841539ab
Koko-Dev <koko.webdev@gmail.com>
2023-10-29 09:19:38 -0400

    DateCounter.js:  Remove console logs from reducer() function

--- src/components/DateCounter.js
+++ src/components/DateCounter.js
@@ -4,9 +4,6 @@ import { useReducer } from 'react';
 const initialState = { count: 0, step: 1 };
 
 function reducer( state, action ) {
-  console.log( 'state from reducer():  ', state );
-  console.log( 'action from reducer():  ', action );
-  
   switch ( action.type ) {
     case 'dec':
       return { ...state, count: state.count - state.step };
