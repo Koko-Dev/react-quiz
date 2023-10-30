30bd37d9b8a8aaed7250adc247005c2221afe3ae
Koko-Dev <koko.webdev@gmail.com>
2023-10-30 14:11:01 -0400

    App.js: We want to store state using the useReducer hook and instead dispatch the type and/or payload to store the data from the questions.json file in state as well as to determine the status depending on what happened, which will be responded to in the reducer(state, action) function in a switch/case"
    // relevant code
    import Header from './components/Header';
    import Main from './components/Main';
    import { useEffect, useReducer } from 'react';
    
    const initialState = {
      questions: [],
    
    //  'loading', 'error', 'ready', 'active', 'finished'
      status: 'loading'
    };
    
    function reducer( state, action ) {
      switch ( action.type ) {
        case 'dataReceived':
          return { ...state, questions: action.payload, status: 'ready' };
        case 'dataFailed':
          return { ...state, status: 'error' };
        default:
          throw new Error( 'Action unknown' );
      }
    }
    
    function App() {
      const [ state, dispatch ] = useReducer( reducer, initialState );
    
    //  Run the data on mount:  []
      useEffect( function () {
        fetch( 'http://localhost:8000/questions' )
            .then( ( res ) => res.json() )
            .then( ( data ) => dispatch( { type: 'dataReceived', payload: data } ) )
            .catch( ( err ) => dispatch( { type: 'dataFailed' } ) );
      }, [] );
    
      return (
          <div className='app'>
            <h1>
              <Header />
              <Main>
                <p>1/15</p>
                <p>Questions</p>
              </Main>
            </h1>
          </div> );
    }
    
    export default App;

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import Header from './components/Header';
 import Main from './components/Main';
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 
+
+const initialState = {
+  questions: [],
+
+//  'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading'
+};
+
+function reducer( state, action ) {
+  switch ( action.type ) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error( 'Action unknown' );
+  }
+}
 
 function App() {
+  const [ state, dispatch ] = useReducer( reducer, initialState );
 
 //  Run the data on mount:  []
   useEffect( function () {
     fetch( 'http://localhost:8000/questions' )
         .then( ( res ) => res.json() )
-        .then( ( data ) => console.log( data ) )
-        .catch( ( err ) => console.log( 'Error' ) );
+        .then( ( data ) => dispatch( { type: 'dataReceived', payload: data } ) )
+        .catch( ( err ) => dispatch( { type: 'dataFailed' } ) );
   }, [] );
   
   return (
