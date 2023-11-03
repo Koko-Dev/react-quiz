23e9f5b6f744cd76bf3262c26c2442419c40ef2c
Koko-Dev <koko.webdev@gmail.com>
2023-11-03 12:53:16 -0400

    App.js:  Add a case for 'start' in the reducer function which returns the state and resets status to 'active';  In the call for the Main component, when the satus is 'ready', StartScreen is called with props for numQuestions and dispatch;  remember the dispatch is one of the variables set to useReducer and when we call dispatch we can set the action type and enter into the switch/case in the reducer function;  Also remember that status: 'ready' is set when the action type is 'dataReceived' which was triggered (dispatched) in the useEffecet;  once the data is received, status becomes 'ready' and the StartScreen is opened with parameters numQuestions and dispatch from useReducer
    StartScreen.js:  When the button is clicked, call dispatch with type: 'start';  this will trigger the reducer function from App.js which will return a renewed state with status 'active'
    
    When the status is active in App.js, return the Question component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useReducer } from 'react';
 import Loader from './components/Loader';
 import Error from './components/Error';
 import StartScreen from './components/StartScreen';
+import Question from './components/Question';
 
 
 const initialState = {
@@ -19,6 +20,10 @@ function reducer( state, action ) {
       return { ...state, questions: action.payload, status: 'ready' };
     case 'dataFailed':
       return { ...state, status: 'error' };
+    case 'start':
+      return {
+        ...state, status: 'active'
+      };
     default:
       throw new Error( 'Action unknown' );
   }
@@ -49,7 +54,12 @@ function App() {
         <Main>
           { status === 'loading' && <Loader /> }
           { status === 'error' && <Error /> }
-          { status === 'ready' && <StartScreen numQuestions={ numQuestions } /> }
+          { status === 'ready' && (
+              <StartScreen
+                  numQuestions={ numQuestions }
+                  dispatch={ dispatch }
+              /> ) }
+          { status === 'active' && <Question /> }
         
         </Main>
       </div> );
