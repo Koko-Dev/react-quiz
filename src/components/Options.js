e350b22dbfc061501e564cc927f5654b2ecd821f
Koko-Dev <koko.webdev@gmail.com>
2023-11-05 12:48:58 -0500

    Options.js:  Reformat

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,5 +1,6 @@
 export default function Options( { question, dispatch, answer } ) {
   const hasAnswered = answer !== null;
+  
   return (
       <div className='options'>
         { question.options.map( ( option, index ) => (
