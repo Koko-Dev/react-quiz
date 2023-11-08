6799dc0b0ab669d33e211708932b31223b807f9f
Koko-Dev <koko.webdev@gmail.com>
2023-11-08 14:36:52 -0500

    NextButton.js:  If the answer is null, return null; but if the index is the same as the number of quetions return a butten which dispatches 'newQuestion' as a type onClick;  the button's text should say 'Next'
    // NextButton.js
    export default function NextButton( { dispatch, answer, numQuestions, index } ) {
      if ( answer === null ) return null;
    
      if ( index < numQuestions - 1 ) return (
          <button
              className='btn btn-ui'
              onClick={ () => dispatch( { type: 'nextQuestion' } ) }
          >
            Next
          </button> );
    }

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,4 +1,11 @@
 export default function NextButton( { dispatch, answer, numQuestions, index } ) {
-  return (
-      <button>Next Button</button> );
+  if ( answer === null ) return null;
+  
+  if ( index < numQuestions - 1 ) return (
+      <button
+          className='btn btn-ui'
+          onClick={ () => dispatch( { type: 'nextQuestion' } ) }
+      >
+        Next
+      </button> );
 }
