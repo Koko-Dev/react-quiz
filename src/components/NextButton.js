ccdcb4ca9063dd1af1594c6c33223324112666d3
Koko-Dev <koko.webdev@gmail.com>
2023-11-10 11:12:39 -0500

    NextButton.js: Reformat

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -9,13 +9,11 @@ export default function NextButton( { dispatch, answer, numQuestions, index } ) 
         Next
       </button> );
   
-  if ( index == numQuestions - 1 )
-    return (
-        <button
-            className='btn btn-ui'
-            onClick={() => dispatch({type: 'finish'})}
-        >
-          Finish
-        </button>
-    )
+  if ( index == numQuestions - 1 ) return (
+      <button
+          className='btn btn-ui'
+          onClick={ () => dispatch( { type: 'finish' } ) }
+      >
+        Finish
+      </button> );
 }
