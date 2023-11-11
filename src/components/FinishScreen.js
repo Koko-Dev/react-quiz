b045ec3977c84e7887011d01951c90d59086a47b
Koko-Dev <koko.webdev@gmail.com>
2023-11-11 13:45:32 -0500

    FinishScreen.js:  Create a var emoji which selects a different emoji based on percentage range;  create p.result which would display this emoji along with the score out of max points;  create p.highscore and a button -- we will fill those in later

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,6 +1,25 @@
 export default function FinishScreen( { points, dispatch, maxPossiblePoints } ) {
   const percentage = (
                          points / maxPossiblePoints ) * 100;
+  
+  let emoji;
+  if ( percentage === 100 ) emoji = '🥇';
+  if ( percentage >= 80 && percentage < 100 ) emoji = '🎉';
+  if ( percentage >= 50 && percentage < 80 ) emoji = '🙃';
+  if ( percentage >= 0 && percentage < 50 ) emoji = '🤨';
+  if ( percentage === 0 ) emoji = '🤦‍♂️';
+  
+  
   return (
-      <p>You scored <strong>{ points }</strong> out of {maxPossiblePoints}</p> );
+      <>
+        <p className='result'>
+          <span>{emoji}</span> You scored <strong>{ points }</strong> out of { maxPossiblePoints }
+        </p>
+        <p className='highscore'></p>
+        <button className='btn btn-ui'></button>
+         );
+        
+        
+      
+      </> );
 }
