bbb2e9528350a25414037b8370cada0973e3881f
Koko-Dev <koko.webdev@gmail.com>
2023-11-14 10:55:40 -0500

    Timer.js:  Add a useEffect that starts logging 'tick' to the console the the app mounts;  this isn't exactly what we want as we don't want the timer to start before we tell it to start, but let's comment out the console log as soon as we see that 'tick' displays to the console every second

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,12 @@
+import { useEffect } from 'react';
+
+
 export default function Timer( { dispatch, secondsRemaining } ) {
+  useEffect( () => {
+    setInterval( () => {
+//      console.log( 'tick' );
+    }, 1000 );
+  }, [] );
   return (
       <div className='timer'>Timer</div> );
 }
